dcaa5b517d5fcd3e048a7da9490167a04b6151c5
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-10 12:19:40 +0100

    useState , added React arrow function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo);
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <span className="heading">To Do App </span>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
-}
+};
 
 export default App;

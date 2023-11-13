2e7a9bd2959a8fac5c14a129fabc1a9d7d71f86d
Pranab kumar Pramanik <pranabcode2023@gmail.com>
2023-11-13 11:39:11 +0100

    edited

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -19,8 +20,9 @@ const App: React.FC = () => {
   console.log(todos);
   return (
     <div className="App">
-      <span className="heading">To Do App </span>
+      <span className="heading">To Do App</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
